e2291cd72f000f75cf499324bd21c7a84d4ee7e6
CodeSalvageON <alexy0730@gmail.com>
2020-12-26 03:17:58 +0000

    Did some stuff

--- public/scripts/phone.js
+++ public/scripts/phone.js
@@ -26,8 +26,32 @@ $("#sms-icon").click(function () {
   }, 2000);
 });
 
+$("#github-icon").click(function () {
+  window.open("https://github.com/CodeSalvageON/splitlevel");
+});
+
+$("#discord-icon").click(function () {
+  window.open("https://discord.com/invite/6HscUEA");
+});
+
+$("#youtube-icon").click(function () {
+  is_other_app_on = true;
+
+  $("#iphone-modal").fadeOut(2000);
+
+  setTimeout(function () {
+    $("#yt-modal").fadeIn(2000);
+  }, 2000);
+});
+
 $("#close-sms-modal").click(function () {
   is_other_app_on = false;
 
   $("#sms-modal").fadeOut(2000);
+});
+
+$("#close-yt-modal").click(function () {
+  is_other_app_on = false;
+
+  $("#yt-modal").fadeOut(2000);
 });
